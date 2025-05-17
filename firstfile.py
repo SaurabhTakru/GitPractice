8b6075863be0dca4b1909aef054d99a327bba1c2
Taken <saurabhtakru@gmail.com>
2025-05-17 15:57:22 +0530

    third change

--- firstfile.py
+++ firstfile.py
@@ -2,4 +2,8 @@ def check():
     print("This is first file")
 
 def check2():
-    print("This is second file")
+    print("This is second file and second graph")
+
+
+def check3():
+    print("This is third file")
